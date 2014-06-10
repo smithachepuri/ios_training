d15b346e5d1d13965f26a8b5956b3014127f7913
Smitha <smithac@yahoo-inc.com>
2014-06-09 20:20:06 -0700

    Changed from error to regular call to rotten tomatoes

--- RottenTomatoes/RottenTomatoes/RottenTomatoes/MoviesViewController.m
+++ RottenTomatoes/RottenTomatoes/RottenTomatoes/MoviesViewController.m
@@ -58,7 +58,7 @@
 -(void)loadMoviesList{
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     
-    NSString *url = @"http://localhost/api/public/v1.0/lists/dvds/top_rentals.json?apikey=g9au4hv6khv6wzvzgt55gpqs";
+    NSString *url = @"http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=g9au4hv6khv6wzvzgt55gpqs";
     
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
